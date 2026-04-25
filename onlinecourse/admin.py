c0339c18c5dd770323404951cfb591b03c42aea4
TheScientist137 <thescientist137@outlook.es>
2026-04-25 10:18:16 -0400

    Task 3

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,42 +1,47 @@
+from django.contrib import admin
 
-	from django.contrib import admin
-	# <HINT> Import any new Models here
-	from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
+# Import models
+from .models import Course, Lesson, Instructor, Learner, Question, Choice, Submission
 
-	# <HINT> Register QuestionInline and ChoiceInline classes here
 
-	class LessonInline(admin.StackedInline):
-		model = Lesson
-		extra = 5
+# Inline classes
+class LessonInline(admin.StackedInline):
+    model = Lesson
+    extra = 5
 
-	class ChoiceInline(admin.StackedInline):
-		model = Choice
-		extra = 2
 
-	class QuestionInline(admin.StackedInline):
-		model = Question
-		extra = 2
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 2
 
-	# Register your models here.
-	class CourseAdmin(admin.ModelAdmin):
-		inlines = [LessonInline]
-		list_display = ('name', 'pub_date')
-		list_filter = ['pub_date']
-		search_fields = ['name', 'description']
 
-	class QuestionAdmin(admin.ModelAdmin):
-		inlines = [ChoiceInline]
-		list_display = ['content']
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 2
 
-	class LessonAdmin(admin.ModelAdmin):
-		list_display = ['title']
 
-	# <HINT> Register Question and Choice models here
+# Admin classes
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [LessonInline]
+    list_display = ('name', 'pub_date')
+    list_filter = ['pub_date']
+    search_fields = ['name', 'description']
 
-	admin.site.register(Course, CourseAdmin)
-	admin.site.register(Lesson, LessonAdmin)
-	admin.site.register(Instructor)
-	admin.site.register(Learner)
-	admin.site.register(Question, QuestionAdmin)
-	admin.site.register(Choice)
-	admin.site.register(Submission)
+
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+    list_display = ['content']
+
+
+class LessonAdmin(admin.ModelAdmin):
+    list_display = ['title']
+
+
+# Register models
+admin.site.register(Course, CourseAdmin)
+admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Instructor)
+admin.site.register(Learner)
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Choice)
+admin.site.register(Submission)
